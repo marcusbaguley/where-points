a968c78364308112c4aba5686ce3629566446ce9
Marcus Baguley <marcus.baguley@gmail.com>
2025-07-14 21:09:30 +1200

    added Time

--- src/utils/tcxBuilder.js
+++ src/utils/tcxBuilder.js
@@ -13,10 +13,35 @@ const tcxCueTypeMap = {
 };
 
 function encode(str) {
-  return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  return (str || "")
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
 }
 
-export function buildTCX(track, coursePoints) {
+export function buildTCX(track, coursePoints, courseName = "where-points route", startTimeISO = null) {
+  // Compute cumulative distance for each track point
+  let distances = [0];
+  for (let i = 1; i < track.length; i++) {
+    const d = window.haversine(
+      track[i - 1].lat,
+      track[i - 1].lon,
+      track[i].lat,
+      track[i].lon
+    );
+    distances.push(distances[i - 1] + d);
+  }
+
+  // Generate times: startTime + X seconds, assuming e.g. 15km/h avg speed
+  const avgSpeed = 4.16; // m/s (~15km/h)
+  const baseTime = startTimeISO ? new Date(startTimeISO) : new Date();
+  let times = [baseTime.toISOString()];
+  for (let i = 1; i < distances.length; i++) {
+    const seconds = distances[i] / avgSpeed;
+    const time = new Date(baseTime.getTime() + seconds * 1000);
+    times.push(time.toISOString());
+  }
+
   return `<?xml version="1.0" encoding="UTF-8"?>
 <TrainingCenterDatabase xmlns="http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -24,36 +49,41 @@ export function buildTCX(track, coursePoints) {
   http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd">
   <Courses>
     <Course>
-      <Name>where-points route</Name>
+      <Name>${encode(courseName)}</Name>
       <Track>
         ${track
           .map(
-            pt => `<Trackpoint>
-              <Position>
-                <LatitudeDegrees>${pt.lat}</LatitudeDegrees>
-                <LongitudeDegrees>${pt.lon}</LongitudeDegrees>
-              </Position>
-              ${pt.ele ? `<AltitudeMeters>${pt.ele}</AltitudeMeters>` : ""}
-            </Trackpoint>`
+            (pt, i) => `<Trackpoint>
+          <Time>${times[i]}</Time>
+          <Position>
+            <LatitudeDegrees>${pt.lat}</LatitudeDegrees>
+            <LongitudeDegrees>${pt.lon}</LongitudeDegrees>
+          </Position>
+          ${pt.ele ? `<AltitudeMeters>${pt.ele}</AltitudeMeters>` : ""}
+          <DistanceMeters>${distances[i].toFixed(1)}</DistanceMeters>
+        </Trackpoint>`
           )
           .join("\n")}
       </Track>
       <CoursePoints>
         ${coursePoints
-          .map(
-            (cp, i) => `<CoursePoint>
-              <Name>${encode(cp.name)}</Name>
-              <Time>${new Date().toISOString()}</Time>
-              <Position>
-                <LatitudeDegrees>${cp.lat}</LatitudeDegrees>
-                <LongitudeDegrees>${cp.lon}</LongitudeDegrees>
-              </Position>
-              <PointType>${
-                tcxCueTypeMap[cp.type] || "Generic"
-              }</PointType>
-              <Notes>${encode(cp.notes || cp.name)}</Notes>
-            </CoursePoint>`
-          )
+          .map(cp => {
+            // Use snapIdx (index of nearest trackpoint) if present, else 0
+            const snapIdx =
+              typeof cp.snapIdx === "number"
+                ? cp.snapIdx
+                : (cp.nearestTrackIdx || 0);
+            return `<CoursePoint>
+          <Name>${encode(cp.name)}</Name>
+          <Time>${times[snapIdx] || times[0]}</Time>
+          <Position>
+            <LatitudeDegrees>${cp.lat}</LatitudeDegrees>
+            <LongitudeDegrees>${cp.lon}</LongitudeDegrees>
+          </Position>
+          <PointType>${tcxCueTypeMap[cp.type] || "Generic"}</PointType>
+          <Notes>${encode(cp.notes || cp.name)}</Notes>
+        </CoursePoint>`;
+          })
           .join("\n")}
       </CoursePoints>
     </Course>
